493697d82abfed2364fe80520c655f9b5c1fd01d
yusuf <hukajavix.com>
2024-10-26 13:07:11 +0300

    First Commit

---
+++ me/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-between gap-4 px-4 lg:px-44 py-3">
+      <img width={150} src={assets.logo} alt="" />
+      <p className="flex-1 border-1 border-gray-400 text-sm text-gray-500 max-sm:hidden">
+        &copy; Huka Yusuf | All rights reserved.
+      </p>
+      <div className="flex gap-1">
+        <img src={assets.facebook_icon} alt="" />
+        <img src={assets.twitter_icon} alt="" />
+        <img src={assets.google_plus_icon} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ server/controllers/userController.js
@@ -0,0 +1,4 @@
+// API CONTROLLER FUNCTION to manage clerk  database
+// http://localhost:4000/api/user/webhooks
+
+const clerkWebhooks = async (reg, res) => {};
